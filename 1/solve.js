dd8ef102794ed43b6253ac9aafe8912c295898c1
Isaac Vallee <ivallee@lush.com>
2020-12-01 09:15:05 -0800

    cleanup and refactor

--- 1/solve.js
+++ 1/solve.js
@@ -3,35 +3,9 @@ const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8').split('\n').map
 
 const testData = ['1721', '979', '366', '299', '675', '1456'].map(Number);
 
-function sum(a, b) {
-
-    if (number === 2020) {
-        return number;
-    } else {
-        sum()
-    }
-}
-
-function main(data) {
-    var arr;
-
-    const answer = data.filter(number => {
-        return data.filter(n => number + n === 2020).length > 0;
-    }).reduce((a, b) => a * b);
-
-    // let sum = data.reduce((a,b) => {
-    //     a = Number(a);
-    //     b = Number(b);
-    //     console.log(`a: ${a}\nb: ${b}`);
-    //     console.log(`a + b = ${a + b}`);
-    //     if (a + b === 2020) {
-    //         return a * b;
-    //     } else {
-    //         return a;
-    //     };
-    // });
-
+function partOne(data) {
+    const answer = data.filter(number => data.filter(n => number + n === 2020).length > 0).reduce((a, b) => a * b);
     console.log(answer);
 };
 
-main(input);
+partOne(input);
