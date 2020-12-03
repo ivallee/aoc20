af4d397385c1d2ce3cc50231411478d37ea015b1
Isaac Vallee <ivallee@lush.com>
2020-12-03 12:39:09 -0800

    d1p1 refactor again

--- 3/solve.js
+++ 3/solve.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
-const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8').trim().split('\n').map(row => row.split(''));
+const input = fs.readFileSync(`${__dirname}/testInput.txt`, 'utf-8').trim().split('\n').map(row => row.split(''));
 
-function traverseSlope([right, down], trees = []) {
-
-    if (down >= input.length) {
-        console.log(trees.length);
+function traverseSlope([x, y], [right, down], trees = []) {
+    x += right;
+    y += down;
+    if (y >= input.length) {
         return trees.length;
     }
-    if (right >= input[down].length) {
-        const diff = right - input[down].length;
-        right = diff;
+    if (x >= input[y].length) {
+        const diff = x - input[y].length;
+        x = diff;
     }
 
-    const currentPosition = input[down][right];
-
+    const currentPosition = input[y][x];
     if (currentPosition === '#' ){
         trees.push(currentPosition)
     }
-
-    traverseSlope([right + 3, down + 1], trees);
+    return traverseSlope([x, y], [right, down], trees);
 }
 
-traverseSlope([3, 1]);
+const part1 = traverseSlope([0,0], [3, 1]);
+
+console.log(part1);
