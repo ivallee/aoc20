f19e8b4e14216dc2d07762fbb2b530869a61d7c5
Isaac Vallee <ivallee@lush.com>
2020-12-02 16:55:07 -0800

    solves d2p2

--- 2/solve.js
+++ 2/solve.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+// const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8').trim().split('\n');
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8').trim().split('\n');
 
 const countChars = (str, char) => {
@@ -6,9 +7,24 @@ const countChars = (str, char) => {
     return str.match(regex) ? str.match(regex).length : 0;
 }
 
-function main(arr) {
-    var answer = 0;
-    var result = arr.map(str => {
+const position = ({ positionOne, positionTwo, char, password }) => {
+    const firstMatch = password[positionOne - 1] === char;
+    const secondMatch = password[positionTwo - 1] === char;
+
+    if (!firstMatch && !secondMatch) {
+        return false
+    } else if (firstMatch && secondMatch) {
+        return false;
+    } else if (firstMatch) {
+        return true;
+    } else if (secondMatch) {
+        return true;
+    }
+};
+
+function partOne(arr) {
+    let answer = 0;
+    const result = arr.map(str => {
         str = str.trim().split(' ');
         return {
             bottomRange: Number(str[0].split('-')[0]),
@@ -19,7 +35,7 @@ function main(arr) {
     });
 
     result.forEach(obj => {
-        var count = countChars(obj.password, obj.char);
+        const count = countChars(obj.password, obj.char);
         if (count <= obj.topRange && count >= obj.bottomRange) {
             answer++;
         }
@@ -29,4 +45,20 @@ function main(arr) {
 
 }
 
-main(input);
+function partTwo(arr) {
+    const result = arr.map(str => {
+        str = str.trim().split(' ');
+        return {
+            positionOne: Number(str[0].split('-')[0]),
+            positionTwo: Number(str[0].split('-')[1]),
+            char: str[1][0],
+            password: str[2],
+        };
+    }).filter(position).length;
+
+    console.log(result);
+
+}
+
+partOne(input);
+partTwo(input);
