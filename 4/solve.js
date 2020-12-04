b5bec03b6d62dde005dc078f8c48909604975b60
ivallee <isaacvallee@gmail.com>
2020-12-04 07:59:20 -0800

    solves d4p1

--- 4/solve.js
+++ 4/solve.js
@@ -2,17 +2,33 @@ const fs = require('fs');
 const file = process.argv.slice(2);
 const input = fs.readFileSync(`${__dirname}/${file}.txt`, 'utf-8').trim();
 
-const passports = input.split('\n\n').map(passport => {
+let reqFields = [];
+const optionalField = 'cid';
+
+const passports = input.split('\n\n').map(fields => {
   const regex = /[\s\n]/;
-  const obj = {};
+  const passport = {};
   
-  passport.split(regex).forEach(field => {
+  fields.split(regex).forEach(field => {
     field = field.split(':');
     const key = field[0];
     const value = field[1];
-    obj[key] = value;
+    passport[key] = value;
+    if (key !== optionalField && !reqFields.includes(key)) {
+      reqFields.push(key);
+    }
   });
-  return obj;
+
+  return passport;
 });
 
-console.log(passports);
+const validPassports = passports.filter(passport => {
+  const passportFields = Object.keys(passport);
+  invalid = reqFields.some(key => {
+    console.log(key !== optionalField && !passportFields.includes(key), key, 'invalid!!!')
+    return key !== optionalField && !passportFields.includes(key)
+  });
+  return !invalid;
+});
+
+console.log(validPassports.length);
