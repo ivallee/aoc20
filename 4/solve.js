cf2cb9943343a63d2a4ec4739da26f99f53e82f2
ivallee <isaacvallee@gmail.com>
2020-12-04 08:05:23 -0800

    cleanup and refactor

--- 4/solve.js
+++ 4/solve.js
@@ -5,30 +5,34 @@ const input = fs.readFileSync(`${__dirname}/${file}.txt`, 'utf-8').trim();
 let reqFields = [];
 const optionalField = 'cid';
 
-const passports = input.split('\n\n').map(fields => {
-  const regex = /[\s\n]/;
-  const passport = {};
-  
-  fields.split(regex).forEach(field => {
-    field = field.split(':');
-    const key = field[0];
-    const value = field[1];
-    passport[key] = value;
-    if (key !== optionalField && !reqFields.includes(key)) {
-      reqFields.push(key);
-    }
+const parsePassports = (inputArr) => {
+  return inputArr.split('\n\n').map(fields => {
+    const regex = /[\s\n]/;
+    const passport = {};
+
+    fields.split(regex).forEach(field => {
+      field = field.split(':');
+      const key = field[0];
+      const value = field[1];
+      passport[key] = value;
+      if (key !== optionalField && !reqFields.includes(key)) {
+        reqFields.push(key);
+      }
+    });
+    return passport;
   });
+}
 
-  return passport;
-});
-
-const validPassports = passports.filter(passport => {
+const validatePassports = (passport) => {
   const passportFields = Object.keys(passport);
   invalid = reqFields.some(key => {
-    console.log(key !== optionalField && !passportFields.includes(key), key, 'invalid!!!')
     return key !== optionalField && !passportFields.includes(key)
   });
   return !invalid;
-});
+};
 
-console.log(validPassports.length);
+function part1(input) {
+  return parsePassports(input).filter(validatePassports).length;
+}
+
+console.log(part1(input));
