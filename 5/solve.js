beccf2d432bdb27d69606b8339c0f085fb9a166e
ivallee <isaacvallee@gmail.com>
2020-12-06 15:53:05 -0800

    semi-solves d5p2

--- 5/solve.js
+++ 5/solve.js
@@ -22,15 +22,31 @@ const partition = (arr, index = 0, start, end) => {
   }
 };
 
-function partOne (input) {
-  const answer = input.map(pass => {
-    const row = partition(pass.row.split(''), 0, 0, 127)
-    const col = partition(pass.col.split(''), 0, 0, 7);
-    return (row * 8) + col;
-  });
+const getSeatIDs = (pass) => {
+  const row = partition(pass.row.split(''), 0, 0, 127)
+  const col = partition(pass.col.split(''), 0, 0, 7);
+  return (row * 8) + col;
+};
 
-  return Math.max(...answer);
+const findMissing = (arr) => {
+  return arr.filter((num, i) => {
+    console.log(num);
+    return num + 1 !== arr[i + 1] && arr[i + 1] !== undefined;
+  });
+};
+
+function partOne(input) {
+  return Math.max(...input.map(getSeatIDs));
 }
 
-console.log(partOne(input));
+function partTwo(input) {
+  let seatIDs = input.map(getSeatIDs).sort((a, b) => a - b);
+
+  return findMissing(seatIDs);
+  // console.log(seatIDs);
+  // return (Math.min(...seatIDs), Math.max(...seatIDs));
+}
+
+// console.log(partOne(input));
+console.log(partTwo(input));
 
