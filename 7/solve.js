3a39a3450d3950f3bdb2b0b0e93322798aff055a
ivallee <isaacvallee@gmail.com>
2020-12-07 22:04:08 -0800

    solves d7p1

--- 7/solve.js
+++ 7/solve.js
@@ -30,12 +30,53 @@ const bagTypes = rules.map(rule => {
     const secondContainedQuantity = matched[2][1];
     bagObject[rootBag][secondContainedBag] = Number(secondContainedQuantity.trim());
   }
+ console.log(matched[3])
+  if (matched[3]) {
+    const thirdContainedBag = matched[3][2];
+    const thirdContainedQuantity = matched[3][1];
+    bagObject[rootBag][thirdContainedBag] = Number(thirdContainedQuantity.trim());
+  }
+
+
+  if (matched[4]) {
+    const fourthContainedBag = matched[4][2];
+    const fourthContainedQuantity = matched[4][1];
+    bagObject[rootBag][fourthContainedBag] = Number(fourthContainedQuantity.trim());
+  }
   return bagObject;
 });
 
-
 const getBag = (arr, bagName) => {
   return arr.find(bag => Object.keys(bag)[0] === bagName);
+};
+
+
+
+const containsTargetBag = (bag, targetBag) => {
+  const bagType = Object.keys(bag)[0];
+  if (Object.keys(bag[bagType]).length === 0) {
+    // console.log('you reached the end my friend', bag);
+    return false;
+  }
+
+  // if (bagType === targetBag) {
+  //   console.log('winner!', bag);
+  //   return true;
+  // }
+  return Object.keys(bag[bagType]).some(containingBag => {
+
+    if (containingBag === targetBag) {
+      // console.log('winner!', bag);
+      return true;
+    }
+    const containingBagObject = getBag(bagTypes, containingBag);
+    // if (Object.keys(containingBagObject)[0] === targetBag) {
+    //   console.log('found!');
+    //   return true;
+    // }
+    return containsTargetBag(containingBagObject, targetBag);
+  });
+  // if (bagObject[bagType][targetBag]) return true;
 };
 
 // function containsTargetBah
@@ -47,4 +88,7 @@ const getBag = (arr, bagName) => {
 
 
 // };
-console.log(getBag(bagTypes, 'shiny gold'));
+// console.log(getBag(bagTypes, 'shiny gold'));
+console.log(bagTypes.filter(bag => {
+  return containsTargetBag(bag, 'shiny gold');
+}).length);
