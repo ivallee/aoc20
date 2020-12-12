b09d29ab174ba251714557ad2233958dd9c83bcb
ivallee <isaacvallee@gmail.com>
2020-12-12 09:04:31 -0800

    solves d11p1

--- 11/solve.js
+++ 11/solve.js
@@ -5,31 +5,32 @@ let input = fs.readFileSync(`${__dirname}/${file}.txt`, 'utf-8')
   .split('\n')
   .map(row => row.split(''));
 
-  Array.prototype.equals = function (array) {
-    // if the other array is a falsy value, return
-    if (!array)
-        return false;
+// deep equals taken from stack overflow
+Array.prototype.equals = function (array) {
+  // if the other array is a falsy value, return
+  if (!array)
+      return false;
 
-    // compare lengths - can save a lot of time 
-    if (this.length != array.length)
-        return false;
+  // compare lengths - can save a lot of time 
+  if (this.length != array.length)
+      return false;
 
-    for (var i = 0, l=this.length; i < l; i++) {
-        // Check if we have nested arrays
-        if (this[i] instanceof Array && array[i] instanceof Array) {
-            // recurse into the nested arrays
-            if (!this[i].equals(array[i]))
-                return false;       
-        }           
-        else if (this[i] != array[i]) { 
-            // Warning - two different object instances will never be equal: {x:20} != {x:20}
-            return false;   
-        }           
-    }       
-    return true;
+  for (var i = 0, l=this.length; i < l; i++) {
+      // Check if we have nested arrays
+      if (this[i] instanceof Array && array[i] instanceof Array) {
+          // recurse into the nested arrays
+          if (!this[i].equals(array[i]))
+              return false;       
+      }           
+      else if (this[i] != array[i]) { 
+          // Warning - two different object instances will never be equal: {x:20} != {x:20}
+          return false;   
+      }           
+  }       
+  return true;
 }
 
-const adjacentSeats = (grid, startX, startY) => {
+const adjacentSeats = (grid, startY, startX) => {
   const directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
   let occupied = 0;
   let empty = 0;
@@ -48,7 +49,7 @@ const adjacentSeats = (grid, startX, startY) => {
   }
   return {
     occupied,
-    empty
+    empty,
   };
 };
 
@@ -56,8 +57,8 @@ const cycleSeats = (grid) => {
   let gridCopy = grid.map(row => row.slice());
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
-      const { occupied, empty } = adjacentSeats(grid, i, j);
       const seat = grid[i][j];
+      const { occupied, empty } = adjacentSeats(grid, i, j);
       if (seat === 'L' && occupied === 0) {
         gridCopy[i][j] = '#';
       } else if (seat === '#' && occupied >= 4) {
@@ -68,21 +69,20 @@ const cycleSeats = (grid) => {
   return gridCopy;
 }
 
-function partOne(grid) {
-  const cycled = cycleSeats(grid);
+const recursiveCycle = (arr) => {
+  const cycled = cycleSeats(arr);
+  if (cycled.equals(arr)) {
+    return arr;
+  }
+  return recursiveCycle(cycled);
+};
 
-  // compare arrays
-  // repeat until stops changing
-  // count occupied seats
-  console.log(cycled)
-  console.log(grid)
-  console.log(grid.equals(grid))
+const countOccurences = (arr, val) => arr.reduce((a, b) => (b === val ? a + 1 : a), 0);
+
+function partOne(grid) {
+  const finalSeats = recursiveCycle(grid);
+  return finalSeats.reduce((a, b) => a + countOccurences(b, '#'), 0);
+ 
 }
 
 console.log(partOne(input));
-
-// copy grid
-  // iterate through first grid while changing copy grid
-
-// a function for counting adjacent seats
-  // if condition, change seat of copy array
